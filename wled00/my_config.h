8eafbd098cee7849964ee083fd1c42bc9c647e25
TurboMan90 <amacri16@gmail.com>
2024-09-09 17:26:10 -0400

    added myconfig

---
+++ wled00/my_config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+
+#define CLIENT_SSID "Boosted Lighting 01"
+#define CLIENT_PASS "boosted24"
+
+
+//#define MAX_LEDS 1500       // Maximum total LEDs. More than 1500 might create a low memory situation on ESP8266.
+//#define MDNS_NAME "wled"    // mDNS hostname, ie: *.local
